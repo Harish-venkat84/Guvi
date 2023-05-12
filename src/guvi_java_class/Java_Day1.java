6530ee67ccb1feff9ff2a3e32dd2ee62d922172e
Harish <harish.venkat2018@gmail.com>
2023-05-12 12:26:17 +0530

    New Repo

---
+++ src/guvi_java_class/Java_Day1.java
@@ -0,0 +1,28 @@
+package guvi_java_class;
+
+public class Java_Day1 {
+	
+	public static void main(String[] args) {
+		
+		boolean pass = true; // 1 bit
+		
+		byte age = 12;  // 8 bits
+		
+		char gender = 'M';   // 16 bits
+		
+		short shirtPrice = 123;   // 16 bits
+		
+		int days = 0;   // 32 bits
+		
+		long teaPrice = 23;   // 64 bits
+		
+		float timeTaken = 12.12f;  // 32 bits
+		
+		double min_Sec = 123.12332;  // 64 bits
+		
+		String str = "Hello_World";
+		
+		
+	}
+
+}
